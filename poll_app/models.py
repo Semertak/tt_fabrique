d53b6d2d7bd3e37324cdef966eb2cd74703a789e
Semertak <govorov-roman@mail.ru>
2021-08-16 22:04:09 +0400

    Add Poll model

--- poll_app/models.py
+++ poll_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+# Poll model
+class Poll(models.Model):
+    title = models.CharField(max_length=25)
+    description = models.TextField(null=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def clean(self):
+        super().clean()
+        if self.start_date > self.end_date:
+            raise ValidationError("EndDate less then StartDate")
