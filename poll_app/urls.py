c98741bdbde3587e8bea2068e3c71c206c75f5a8
Semertak <govorov-roman@mail.ru>
2021-08-21 21:23:46 +0400

    Add another CRUD-methods for Question entity

--- poll_app/urls.py
+++ poll_app/urls.py
@@ -7,5 +7,6 @@ app_name = "polls"
 urlpatterns = [
     path("polls/", PollView.as_view()),
     path("polls/<int:pk>", PollView.as_view()),
-    path("polls/<int:p_pk>/questions/", QuestionsView.as_view())
+    path("polls/<int:p_pk>/questions/", QuestionsView.as_view()),
+    path("polls/<int:p_pk>/questions/<int:q_pk>", QuestionsView.as_view())
 ]
